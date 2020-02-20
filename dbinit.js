579782483a5dc5f42995c88b45dc9134c04d2da8
Marosi Tibor <marositibor@Marosi-MacBook-Air.local>
2020-02-20 20:57:50 +0100

    inventory endpoint added

--- dbinit.js
+++ dbinit.js
@@ -1,15 +1,26 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('inventory.db')
  
-function database(namepm, categorypm){
-    db.serialize(function() {
-        db.run("CREATE TABLE products ( name VARCHAR(100), category VARCHAR(60))")
-     
-        db.prepare('INSERT INTO products VALUES (?, ?)')
-            .run(`${namepm}`, `${categorypm}`)
-    
-    
-    });
-}
+db.serialize(function() {
+    db.run("CREATE TABLE products (name VARCHAR(100), category VARCHAR(60))")
+ 
+    db.prepare('INSERT INTO products VALUES (?, ?)')
+        .run('Processzor', 'Számítástechnika')
 
-database("vasalo", "haztartas")
+    db.prepare('INSERT INTO products VALUES (?, ?)')
+        .run('Memória', 'Számítástechnika')
+
+    db.prepare('INSERT INTO products VALUES (?, ?)')
+        .run('Billentyűzet', 'Számítástechnika')
+
+    db.prepare('INSERT INTO products VALUES (?, ?)')
+        .run('Mosogatógép', 'Konyhatechnika')
+
+    db.prepare('INSERT INTO products VALUES (?, ?)')
+        .run('Elektromos radiátor', 'Fűtéstechnika')
+
+    db.prepare('INSERT INTO products VALUES (?, ?)')
+        .run('Egér', 'Számítástechnika')
+
+});
+ 
--- inventorysrv.js
+++ inventorysrv.js
@@ -2,13 +2,17 @@ const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
 const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+
+
 const db = new sqlite3.Database('inventory.db')
 const PORT = 3000;
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('handlebars', hbs());
 app.set('view engine', 'handlebars');
-
-const items = [];
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -16,19 +20,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 	res.render('home', { items});
 });*/
 
-app.get('/', (req, res) => {
+app.get('/products', (req, res) => {
     db.serialize(function() {
         db.all("SELECT rowid, name, category from products", function(err, results) {
             if (err != null) {
                 res.send("Missing from database")
             }
-			items.push(results)
-			console.log(results)
-          res.render('home', {items:results})
-            
+          res.render('products', {
+            items:results,
+            products:true,
+            title:"Termékek"})
         });
       });
+})
 
+app.get('/inventory', (req, res) => {
+    db.serialize(function() {
+        db.all("SELECT rowid, name, category from products", function(err, results) {
+            if (err != null) {
+                res.send("Missing from database")
+            }
+          res.render('inventory', {
+            items:results,
+            inventory:true,
+            title:"Készletek"})
+        });
+      });
+})
+
+app.post('/products', (req,res) => {
+    const {name,category} = req.body;
+    db.serialize(function() {
+        db.prepare('INSERT INTO products VALUES (?, ?)')
+        .run(name, category)
+        res.redirect('/products')
+      });
 })
 
 app.listen(PORT, () => console.log(`App is started and listening on port ${PORT}`));
